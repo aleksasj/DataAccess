9476366e596368739de4d94021558b943d17f8f8
Aleksas Janulevičius <aseksas@gmail.com>
2023-02-13 16:17:32 +0200

    Some improvements :)

--- Repositories/IOrderRepository.cs
+++ Repositories/IOrderRepository.cs
@@ -7,7 +7,9 @@ public interface IOrderRepository
     Task Assign(int orderId, int driverId);
     Task Cancel(int orderId);
     Task Create(string name, string phone, int pickupId, int destinationId, string comment = "");
-    Task<UsersModel?> Detail(int orderId);
+    Task<OrdersModel?> Detail(int orderId);
     Task Finish(int orderId);
     Task Picked(int orderId);
+    Task<IEnumerable<OrdersModel>> GetOrders(int? driverId = null, int page = 1, int[] status = null, int perPage = 10);
+
 }
--- Repositories/OrderRepository.cs
+++ Repositories/OrderRepository.cs
@@ -25,11 +25,34 @@ public class OrderRepository : IOrderRepository
     public Task Finish(int orderId) =>
        _db.SaveData("dbo.spOrder_Finish", new { Id = orderId });
 
-    public async Task<UsersModel?> Detail(int orderId)
+    public async Task<OrdersModel?> Detail(int orderId)
     {
-        var result = await _db.LoadData<UsersModel, dynamic>("dbo.spOrder_Detaip", new { Id = orderId });
+        var result = await _db.LoadData<OrdersModel, dynamic>("dbo.spOrder_Detail", new { Id = orderId });
 
         return result.FirstOrDefault();
     }
 
+    public async Task<IEnumerable<OrdersModel>> GetOrders(int? driverId = null, int page = 1, int[]? status = null, int perPage = 10)
+    {
+        page = page < 0 ? 1 : page;
+        perPage = perPage < 1 ? 1 : 10;
+        page--;
+
+        if (status == null) {
+            status = new int[] { 
+                OrdersModel.STATUS_CANCELED,
+                OrdersModel.STATUS_NEW,
+                OrdersModel.STATUS_ASSIGNED,
+                OrdersModel.STATUS_PICKED,
+                OrdersModel.STATUS_FINISHED
+            };
+        }
+
+        if (driverId != null)
+        {
+            return await _db.LoadData<OrdersModel, dynamic>("dbp.spOrder_ListByUser", new { DriverId = driverId, Offset = page, Status = string.Join(",", status), Limit = perPage });
+        }
+
+        return await _db.LoadData<OrdersModel, dynamic>("dbp.spOrder_List", new { Offset = page, Status = string.Join(",", status), Limit = perPage });
+    }
 }
