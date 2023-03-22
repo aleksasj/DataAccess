a405e65c53e0ea67068541609542f95ae00bb9ae
Aleksas Janulevičius <aseksas@gmail.com>
2023-03-22 18:58:17 +0200

    Service provider for DataAccess

---
+++ DataAccessServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+using DataAccess.Data;
+using DataAccess.DbAccess;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess;
+
+public static class DataAccessServiceExtension
+{
+    public static IServiceCollection AddDataAccess(this IServiceCollection services)
+    {
+        services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        services.AddSingleton<IUserRepository, UserRepository>();
+        services.AddSingleton<IAddressRepository, AddressRepository>();
+        services.AddSingleton<IDriverRepository, DriverRepository>();
+        services.AddSingleton<IOrderRepository, OrderRepository>();
+
+        return services;
+    }
+}
